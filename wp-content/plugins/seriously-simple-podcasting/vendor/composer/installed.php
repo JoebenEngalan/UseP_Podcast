3d91b1323248cd4faaeb472840ae470a1e7433e4
tron1993 <trone1993@gmail.com>
2020-12-01 15:05:28 +0800

    commit

---
+++ wp-content/plugins/seriously-simple-podcasting/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '30248242b95827929ec3207dc34863a5307ec343',
+    'name' => 'castos/seriously-simple-podcasting',
+  ),
+  'versions' => 
+  array (
+    'castos/seriously-simple-podcasting' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '30248242b95827929ec3207dc34863a5307ec343',
+    ),
+  ),
+);
